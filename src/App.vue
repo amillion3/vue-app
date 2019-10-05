64151ba555561e6b96115296bce6be8fc05045f7
Andy Million <andy@summit-education.com>
2019-10-05 13:59:29 -0500

    Setup EmployeeTable.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+    <!-- Passing 'employees' as a prop... -->
+    <employee-table v-bind:employees="employees"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // Using the '@', specifies the `src` directory
+  import EmployeeTable from '@/components/EmployeeTable.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      EmployeeTable,
+    },
+    data() {
+      return {
+        employees: [
+          {
+            id: 1,
+            name: 'Ben Smith',
+            email: 'bsmith@gmail.com',
+          },
+          {
+            id: 2,
+            name: 'Steve Brennon',
+            email: 'sbrennon@gmail.com',
+          },
+          {
+            id: 3,
+            name: 'Richard Sully',
+            email: 'rsully@gmail.com',
+          },
+        ],
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
+
 </style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+      <tr>
+        <th>Employee name</th>
+        <th>Employee email</th>
+      </tr>
+      </thead>
+      <tbody>
+      <!-- Vue requires unique ids for any element in an array -->
+      <tr v-for="employee in employees" :key="employee.id">
+        <td>{{ employee.name }}</td>
+        <td>{{ employee.email }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  // In Vue, the convention is that the filename and import will be in PascalCase, such as EmployeeTable,
+  //  but when used in a template, this will translate to kebab-case, <employee-table>
+  export default {
+    name: "employee-table",
+    props: {
+      employees: Array, // Incoming props, an array named `employees`
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
